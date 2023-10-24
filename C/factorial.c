e49504d5b51ed1a995ab93c697a31e7a1c257a13
rajvardhan75 <147995852+rajvardhan75@users.noreply.github.com>
2023-10-24 15:25:22 +0530

    Update factorial.c
    Optimized

--- C/factorial.c
+++ C/factorial.c
@@ -1,24 +1,25 @@
 #include<stdio.h>
+int factorial_lookup_table[] = {1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880};
 
-int fact(int num);
-int fact (int num){
-  if(num<=1){
-    return 1;
+int fact(int num) {
+  if (num < 10) {
+    return factorial_lookup_table[num];
   } else {
-    return num*fact(num-1);
+    return num * fact(num - 1);
   }
 }
 
-int main ()
-{
+int main() {
   int num;
-  printf ("Enter number for factorial \n");
-  scanf ("%d", &num);
-  if(num>=0){
-    int f= fact (num);
-    printf("Factorial is: %d",f);
+  printf("Enter number for factorial: ");
+  scanf("%d", &num);
+
+  if (num >= 0) {
+    int f = fact(num);
+    printf("Factorial is: %d\n", f);
   } else {
-    printf("Factorial of a negative number does not exist");
+    printf("Factorial of a negative number does not exist\n");
   }
+
   return 0;
 }
