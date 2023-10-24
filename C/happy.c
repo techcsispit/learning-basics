e0b645b211f0f623329c3fd04399665230432756
rajvardhan75 <147995852+rajvardhan75@users.noreply.github.com>
2023-10-24 10:32:28 +0530

    Update happy.c

--- C/happy.c
+++ C/happy.c
@@ -5,6 +5,5 @@ printf("#   # #   # #   # #   #  # # \n");
 printf("##### ##### ##### #####   #  \n");
 printf("#   # #   # #     #       #  \n");
 printf("#   # #   # #     #       #  \n");
-printf("\n");
-
+return 0;
 }
