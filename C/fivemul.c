92d1c9e21f81ad249d9e7d0c0a212c7c84b5b5a7
Saayna <148362266+Saayna@users.noreply.github.com>
2023-10-24 23:35:24 +0530

    Update fivemul.c
    This code works as it decrements the value till -4 and then increments to 11 .Also we can take any input value .

--- C/fivemul.c
+++ C/fivemul.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
+int c;
+int flag;
+int pattern(int n)
+{
+    if(n > 0 && flag == 0)
+    {
+        printf("%d ", n);
+        pattern (n - 5);
+        
+    }
+    else if (n <= c)
+    {
+        printf("%d ", n);
+        flag = 1; 
+        pattern (n + 5);
+    }
+}
+int main()
+{
+    int n;
+    printf("Enter the value of n \n");
+    scanf("%d", &n);
+    c = n;
+    pattern(n);
+}
 
-int five(int n){
-	if(n>0){
-		printf("%d\t", n);
-		five(n-5);
-	}
-	printf("%d\t", n);	
-}
-int main(){
-	int n;
-	printf("Enter N: ");
-	scanf("%d", &n);
-	five(n);
-	printf("\n");
-}
+
+
