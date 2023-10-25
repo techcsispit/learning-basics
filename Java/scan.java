abee7b170dcfde38d7b1a785b93906e345004701
piyushr6 <145590327+piyushr6@users.noreply.github.com>
2023-10-25 09:00:14 +0530

    Update scan.java
    1. Added a prompt message like "enter a number"  because the output terminal did not  show anything like what should be entered
    
    2. Added a display message as well like Number entered is..
    
    3. Added another prompt message to enter a sentence.
    
    4. The first word was getting printed right beside the last line. So, added an extra SOPln in line 15 to move control to next line, by which all words that are split can be printed below one another.

--- Java/scan.java
+++ Java/scan.java
@@ -2,15 +2,17 @@ import java.util.*;
 public class scan {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Enter a number:");
             int a = scanner.nextInt();
-            System.out.println(a);
+            System.out.println("Number entered is "+a);
 
             // buffer removal
+            System.out.println("Enter a sentence which will then be displayed as split of words:");
             scanner.nextLine();
-
+            
             String str = scanner.nextLine();
             System.out.println(str);
-
+            System.out.println();
             String[] words = str.split(" ");
             System.out.println(Arrays.toString(words));
 
