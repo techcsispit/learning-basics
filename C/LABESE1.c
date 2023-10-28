5d7a9a36c64ef254d6179ca117534fcb67aaff4a
rajvardhan75 <147995852+rajvardhan75@users.noreply.github.com>
2023-10-28 11:34:29 +0530

    Update LABESE1.c
    In the sale[i] = round(arr[i]*0.9) - 0.01 line, this code is rounding the result of the multiplication to the nearest pound before subtracting the 1 penny. This means that if the original price is a multiple of 100, the sale price will always be less than the original price. If the sale price is negative, then the calculation is invalid and you should output an error message.

--- C/LABESE1.c
+++ C/LABESE1.c
@@ -1,40 +1,34 @@
-#include <stdio.h>
-#include <math.h>
-
-// Write a program which inputs N followed by the prices of N items. The program should
+// Write a program that inputs N followed by the prices of N items. The program should
 // output the price at which each item is offered in a sale. The sale price is calculated as follows.
 // The original price is reduced by 10%, the resulting quantity raised to the nearest pound and then
 // 1 penny subtracted from it. If the resulting quantity is less than the original price then the new
-// price is output, otherwise the old price is output together with a warning message.
-
-int main(){
-    int n;
-    printf("Enter N: ");
-    scanf("%d", &n);
-
-    float arr[n];
-    for(int i = 0; i<n; i++){
-        printf("Enter Original Price of %d item: ", i+1);
-        scanf("%f", &arr[i]);
+// price is output, otherwise, the old price is output together with a warning message.
+#include <stdio.h>
+#include <math.h>
+int main() {
+  int n;
+  printf("Enter N: ");
+  scanf("%d", &n);
+ float arr[n];
+  for (int i = 0; i < n; i++) {
+    printf("Enter Original Price of %d item: ", i + 1);
+    scanf("%f", &arr[i]);
+  }
+ float sale[n];
+  for (int i = 0; i < n; i++) {
+    sale[i] = round(arr[i] * 0.9 - 0.01);
+  }
+  printf("\nITEM\tOriginal Price\tSale Price\n");
+  for (int i = 0; i < n; i++) {
+    if (sale[i] < 0) {
+      printf("%-4d\t%14.2f\t%10.2f - ERROR\n", i, arr[i], sale[i]);
+      continue;
     }
-
-    float sale[n];
-    for(int i = 0; i<n; i++){
-        sale[i] = round(arr[i]*0.9) - 0.01;
+    if (sale[i] < arr[i]) {
+      printf("%-4d\t%14.2f\t%10.2f\n", i, arr[i], sale[i]);
+    } else {
+      printf("%-4d\t%14.2f\t%10.2f - ERROR\n", i, arr[i], sale[i]);
     }
-    
-    printf("\nITEM\tOriginal Price\tSale Price\n");
-    for(int i = 0; i<n; i++){
-        if(sale[i]<0){
-            printf("%-4d\t%14.2f\t%10.2f - ERROR\n", i, arr[i], sale[i]);
-            continue;
-        }
-        
-        if(sale[i]<arr[i]){
-            printf("%-4d\t%14.2f\t%10.2f\n", i, arr[i], sale[i]);
-        }else{
-            printf("%-4d\t%14.2f\t%10.2f - ERROR\n", i, arr[i], sale[i]);
-        }
-    }
-
+  }
+  return 0;
 }
