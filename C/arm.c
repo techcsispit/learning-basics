3a00fd92255bd08833318b3a3930b71edb8aac79
rajvardhan75 <147995852+rajvardhan75@users.noreply.github.com>
2023-10-28 12:43:54 +0530

    Update arm.c
    The problem with this code is that the pow function in the Finding Sum loop is not correctly calculating the power of the individual digits. The pow function takes two arguments: the base and the exponent. In this code, you are passing the digit itself as both the base and the exponent. This is incorrect because the exponent should be the number of digits in the input number.

--- C/arm.c
+++ C/arm.c
@@ -1,28 +1,35 @@
 #include <stdio.h>
 #include <math.h>
+
+int pow(int base, int exponent, int num_digits) {
+  int result = 1;
+  for (int i = 0; i < exponent; i++) {
+    result *= base;
+  }
+  return result;
+}
+
 int main() {
-    int num, n=0, sum=0;
-    printf("Enter a Number: ");
-    scanf("%d", &num);
-    int num2 = num;
-    int finnumcheck = num;
-    
-    //Finding Count
-    while(num2!=0){
-        num2 /=10;
-        n++;
-    }
-    
-    //Finding Sum
-    while (num!=0){
-        int x = num%10;
-        sum+=pow(x, n);
-        num = num/10;
-    }
-    //Checking for Armstrong Num
-    if (finnumcheck != sum){
-        printf("%d is not an Armstrong Number\n", finnumcheck);
-    }else{
-        printf("%d is an Armstrong Number\n", finnumcheck);
-    }
+  int num, n = 0, sum = 0;
+  printf("Enter a Number: ");
+  scanf("%d", &num);
+  int num2 = num;
+  int finnumcheck = num;
+
+  while (num2 != 0) {
+    num2 /= 10;
+    n++;
+  }
+  while (num != 0) {
+    int x = num % 10;
+    sum += pow(x, n, n);
+    num /= 10;
+  }
+  if (finnumcheck != sum) {
+    printf("%d is not an Armstrong Number\n", finnumcheck);
+  } else {
+    printf("%d is an Armstrong Number\n", finnumcheck);
+  }
+
+  return 0;
 }
